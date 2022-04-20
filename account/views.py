c644dda35ef9c56e8b445154457a223603d53573
Petro <saliypetro22@gmail.com>
2022-04-20 06:23:13 -0700

    Login and Update/Delete profile functionality

--- account/views.py
+++ account/views.py
@@ -7,8 +7,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .token import account_activation_token
 
@@ -16,12 +15,34 @@ from .token import account_activation_token
 @login_required
 def dashboard(request):
     return render(request,
-                  'account/user/dashboard.html',
+                  'account/user/dashboard.html'
                   )
 
 
-def account_register(request):
+@login_required
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
 
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+
+    return render(request,
+                  'account/user/edit_details.html', {'user_form': user_form})
+
+
+@login_required
+def delete_user(request):
+    user = UserBase.objects.get(user_name=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirmation')
+
+
+def account_register(request):
     if request.user.is_authenticated:
         return redirect('account:dashboard')
 
@@ -42,7 +63,7 @@ def account_register(request):
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
-            return HttpResponse('registered succesfully and activation sent')
+            return HttpResponse('registered successfully and activation sent')
     else:
         registerForm = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': registerForm})
